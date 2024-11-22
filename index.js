be05524de3779b203f0d5a2db099350b996ed614
Ben Barefield <ben.barefield@gmail.com>
2024-11-22 10:42:59 -0800

    path arguments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const {readFile, writeFile} = require("node:fs/promises");
 const path = require("node:path");
 
-async function updatePackage() {
-  const localData = await readFile(path.resolve(__dirname, "./package.json"));
+async function updatePackage(localPackagePath, rootPackagePath) {
+  const localData = await readFile(localPackagePath);
   const localPackage = JSON.parse(localData);
-  const rootData = await readFile(path.resolve(__dirname, "../../package.json"));
+  const rootData = await readFile(rootPackagePath);
   const rootPackage = JSON.parse(rootData);
 console.log("local\n", localPackage)
   if(localPackage.watch) {
